557fdc495b45589b75921c97e8cf49b0514553e7
v-alex-dev <vensalex1991@gmail.com>
2023-12-29 17:42:14 +0100

    Add description && start describe formations

--- src/components/titleSection.jsx
+++ src/components/titleSection.jsx
@@ -1,14 +1,41 @@
 import useWidthScreen from "../hooks/w-screen.js";
 
-const TitleSection = ({title, urlImg}) => {
+const TitleSection = ({}) => {
 
     const screenWidth = useWidthScreen()
 
 
     return (
-        <div>
-            <img src="" alt=""/>
-            <p></p>
+        <div className={"flex flex-row md:w-full gap-4 items-center justify-between"}>
+            <img src={'./src/assets/PhotCV.jpg'} alt={"Alexandre"} className={"md:w-full"}/>
+            <div className={"text-left"}>
+                <p>
+                    Ayant commencé ma carrière professionnelle en tant que chauffeur, ma passion pour l'informatique m'a
+                    conduit à explorer le domaine du développement web. Mon initiation a débuté dans le back end,
+                    notamment
+                    avec l'environnement .NET C#, où j'ai acquis une solide compréhension des rouages techniques.
+                </p>
+                <p>Mon engagement envers le développement web s'est consolidé lors d'un stage chez Barestho, où j'ai eu
+                    l'opportunité de plonger dans une expérience full stack en utilisant Django pour le back end et
+                    React pour le front end. Cette immersion complète m'a permis de développer mes compétences de
+                    manière holistique, en comprenant l'ensemble du processus de développement d'applications web, de la
+                    conception à la mise en œuvre.
+                </p>
+                <p>
+                    Fort de cette expérience, j'ai décidé de concentrer mes efforts sur le renforcement de mes
+                    compétences front-end. C'est à ce stade que j'ai rejoint la formation BeCode à Charleroi, cherchant
+                    à approfondir ma compréhension de la conception visuelle et interactive. Cette décision stratégique
+                    a été motivée par ma volonté de maîtriser pleinement l'aspect visuel des applications web et
+                    d'offrir des expériences utilisateur exceptionnelles.
+                </p>
+                <p>
+                    Mon parcours, bien que non conventionnel, reflète ma détermination à élargir mes horizons dans le
+                    domaine du développement web, passant du back end au full stack et se concentrant ensuite sur le
+                    front end. Mon portfolio témoigne de cette évolution, illustrant mes compétences acquises à chaque
+                    étape de ce parcours diversifié.
+                </p>
+            </div>
+
         </div>
     )
 }
--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -1,23 +1,23 @@
 import Article from "../components/article.jsx";
 import {useEffect, useState} from "react";
 import {motion} from "framer-motion";
-import"../../public/descriptionTitle.svg";
+import "../../public/descriptionTitle.svg";
 import TitleSection from "../components/titleSection.jsx";
+
 const Home = () => {
     const [loading, setLoading] = useState(true);
 
-    const articleContent = [
-        {
-            parcours: "Technocité",
-            url: 'https://technocite.be/',
-            description: 'test'
-        },
-        {
-            parcours: "Becode",
-            url:'https://becode.org/fr/',
-            description: 'test2'
-        }
-    ]
+    const articleContent = [{
+        parcours: "Technocité",
+        url: 'https://technocite.be/',
+        description: '\n' + 'Formation dynamique de 6 mois en développement .NET, mettant l\'accent sur le langage C# et' +
+            ' les fondements back end tels que ASP, ADO, SQL, et Entity Framework Core. Expertise solide en ' +
+            'administration SQL Server, méthodologies Scrum, et utilisation avancée de Git. Exploration du ' +
+            'développement cross-platform avec Xamarin et du cloud Azure. Projet final : conception et défense d\'une ' +
+            'application devant des professionnels, démontrant une maîtrise complète du développement web.'
+    }, {
+        parcours: "Becode", url: 'https://becode.org/fr/les-formations/junior-developer/', description: 'test2'
+    }]
 
     useEffect(() => {
         const fetchData = async () => {
@@ -33,27 +33,23 @@ const Home = () => {
         fetchData();
     }, []);
 
-    return (
-        <div id="home" className={"w-full"}>
+    return (<div id="home" className={"w-full"}>
             <section id={"description"}
-                    className={"bg-white rounded md:p-4"}>
-                <TitleSection title={"description"} urlImg={"../../public/descriptionTitle.svg"}/>
+                     className={"bg-white rounded md:p-4"}>
+                <TitleSection/>
                 <div id={"Parcours"} className={" md:grid md:grid-cols-2 gap-2"}>
-                    {articleContent.map((item, index) => (
-                        <motion.a
+                    {articleContent.map((item, index) => (<motion.a
                             initial={{opacity: -1, scale: 0}}
                             animate={{opacity: 1, scale: 1}}
                             transition={{
-                                ease: "easeOut",
-                                type: "tween",
-                                duration: 1,
+                                ease: "easeOut", type: "tween", duration: 1,
                             }}
                             href={item.url}
                             key={index}
+                            target={"_blank"}
                             className={"bg-light-orange rounded-xl shadow-dark p-4 hover:shadow-2xl duration-150"}>
                             <Article title={item.parcours} description={item.description} loading={loading}/>
-                        </motion.a>
-                    ))}
+                        </motion.a>))}
                 </div>
             </section>
             <section id={"skills"}>
@@ -62,8 +58,7 @@ const Home = () => {
             <section id={"soft-skills"}>
                 <h2>soft-skills</h2>
             </section>
-        </div>
-    )
+        </div>)
 }
 
 export default Home;
